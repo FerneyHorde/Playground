0d2d7f5d7aa9c84e640144fc8174b526774afede
Robert Ferney <capnregex@gmail.com>
2014-01-23 18:01:24 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playground::Application.config.secret_key_base = '49beb386e2c355e9e967d3b2e67243a0df3cfd8cb1c582d9e36adc56e8fd544120891609787f05ad515d50e423b2b30c5eee0e96b3f1923df0ee09c56dd695a7'
